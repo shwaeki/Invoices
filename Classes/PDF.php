3b23f1365d83e548e7f616be0ca00379ac71367d
shwaeki <shwaeki@hotmail.com>
2022-01-17 21:59:56 +0200

    ss

--- Classes/PDF.php
+++ Classes/PDF.php
@@ -11,8 +11,8 @@
 namespace ConsoleTVs\Invoices\Classes;
 
 use Dompdf\Dompdf;
-use Dompdf\Options;
 use Illuminate\Support\Facades\View;
+use Meneses\LaravelMpdf\Facades\LaravelMpdf as PDF;
 
 /**
  * This is the PDF class.
@@ -35,26 +35,9 @@ class PDF
     {
         $template = strtolower($template);
 
-        $options = new Options();
-
-        $options->set('isRemoteEnabled', true);
-        $options->set('isPhpEnabled', true);
-
-        $pdf = new Dompdf($options);
-
-        $context = stream_context_create([
-            'ssl' => [
-                'verify_peer'      => false,
-                'verify_peer_name' => false,
-                'allow_self_signed'=> true,
-            ],
-        ]);
-
-        $pdf->setHttpContext($context);
 
         $GLOBALS['with_pagination'] = $invoice->with_pagination;
-
-        $pdf->loadHtml(View::make('invoices::'.$template, ['invoice' => $invoice]));
+		$pdf = PDF::loadView(View::make('invoices::'.$template, ['invoice' => $invoice]));
         $pdf->render();
 
         return $pdf;
